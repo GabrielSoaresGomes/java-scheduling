5092265de6943d61a2cc7a3b7a30c630fa17fea7
Soares <gabrielsoares221@gmail.com>
2024-06-02 13:35:58 -0300

    e

--- Frontend/src/components/Navbar/Navbar.js
+++ Frontend/src/components/Navbar/Navbar.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Navbar.css"
 import {NavLink} from "react-router-dom";
+import menu from "../../static/menu_24dp_FILL0_wght400_GRAD0_opsz24.svg"
 
 const Navbar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  }
+
   return (
     <nav className="navbar">
-      <div className="navbar-brand">Agendamentos</div>
+      <div className="navbar-brand">Agendamento</div>
       <div className="navbar-links">
-          <NavLink to={"/"}> Adicionar Usuários</NavLink>
-          <NavLink to={"user/add"}> Listar Usuários </NavLink>
+          <div className="dropdown">
+              <button className="dropbtn" onClick={toggleDropdown}>
+                <img src={menu} alt={"MenuIcon"} />
+              </button>
+              {dropdownOpen && (
+                  <div className="dropdown-content">
+                      <NavLink to="/user/add">Adicionar Usuário</NavLink>
+                      <NavLink to="/">Listar Usuários</NavLink>
+                  </div>
+              )}
+          </div>
       </div>
     </nav>
   );
